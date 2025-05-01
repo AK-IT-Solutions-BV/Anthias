59d187279589b9ee545890a5bc117b6515be14f7
Olcay-1018693 <1018693@hr.nl>
2025-05-01 12:00:45 +0200

    Add new SVG logos and update Docker build scripts
    - Added new logo SVG files: logo-text.svg and logo.svg to the assets/images directory.
    - Updated build_and_push.sh to create a Docker buildx instance if it doesn't exist and changed image tags to "latest-arm".
    - Modified docker-compose.yml.tmpl to use the new "latest-arm" tag for all services instead of "latest-pi4".

---
+++ backup/restore_files.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Pad naar de backup-directory
+BACKUP_DIR="/home/admin/backup/screenly"
+
+# Bestanden/directories kopiëren met sudo
+sudo cp "$BACKUP_DIR/docker-compose.yml.tmpl" /home/admin/screenly/docker-compose.yml.tmpl
+sudo cp "$BACKUP_DIR/bin/start_server.sh" /home/admin/screenly/bin/start_server.sh
+sudo cp -r "$BACKUP_DIR/templates/" /home/admin/screenly/
+sudo cp -r "$BACKUP_DIR/static/img/" /home/admin/screenly/static/
+sudo cp -r "$BACKUP_DIR/static/favicons/" /home/admin/screenly/static/
+sudo cp "$BACKUP_DIR/static/sass/_variables.scss" /home/admin/screenly/static/sass/_variables.scss
+sudo cp "$BACKUP_DIR/static/sass/_custom-bootstrap.scss" /home/admin/screenly/static/sass/_custom-bootstrap.scss
+sudo cp -r "$BACKUP_DIR/staticfiles/img/" /home/admin/screenly/staticfiles/
+sudo cp -r "$BACKUP_DIR/staticfiles/favicons/" /home/admin/screenly/staticfiles/
+sudo cp "$BACKUP_DIR/staticfiles/sass/_variables.scss" /home/admin/screenly/staticfiles/sass/_variables.scss
+sudo cp "$BACKUP_DIR/staticfiles/sass/_custom-bootstrap.scss" /home/admin/screenly/staticfiles/sass/_custom-bootstrap.scss
+sudo cp "$BACKUP_DIR/ansible/roles/splashscreen/files/splashscreen.png" /home/admin/screenly/ansible/roles/splashscreen/files/splashscreen.png
+sudo cp -r "$BACKUP_DIR/website/assets/images/" /home/admin/screenly/website/assets/
+
+echo "Alle bestanden zijn succesvol gekopieerd met sudo."
--- build_and_push.sh
+++ build_and_push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Enable buildx
-docker buildx use --name multiarch --use || true
+docker buildx use --name multiarch --use || docker buildx create --name multiarch --use
 
 # Login to Docker Hub
 docker login
@@ -13,21 +13,25 @@ ENVIRONMENT=production ./bin/generate_dev_mode_dockerfiles.sh
 build_and_push() {
     local dockerfile=$1
     local imagename=$2
-    echo "Building and pushing $imagename..."
-    docker buildx build --platform linux/arm64/v8 \
+    local tag=$3
+    echo "Building and pushing $imagename with tag $tag..."
+    docker buildx build --platform linux/arm64/v8,linux/arm/v8 \
         -f "$dockerfile" \
-        -t "zortos/$imagename:latest-pi4" \
+        -t "zortos/$imagename:$tag" \
         --no-cache \
         --push .
 }
 
-# Build and push all images
-build_and_push "docker/Dockerfile.server" "anthias-server"
-build_and_push "docker/Dockerfile.viewer" "anthias-viewer"
-build_and_push "docker/Dockerfile.nginx" "anthias-nginx"
-build_and_push "docker/Dockerfile.websocket" "anthias-websocket"
-build_and_push "docker/Dockerfile.celery" "anthias-celery"
-build_and_push "docker/Dockerfile.redis" "anthias-redis"
-build_and_push "docker/Dockerfile.wifi-connect" "anthias-wifi-connect"
+# Define the tag
+TAG="latest-arm"
 
-echo "All images have been built and pushed successfully!" 
+# Build and push all images
+build_and_push "docker/Dockerfile.server" "anthias-server" "$TAG"
+# build_and_push "docker/Dockerfile.viewer" "anthias-viewer" "$TAG"
+build_and_push "docker/Dockerfile.nginx" "anthias-nginx" "$TAG"
+build_and_push "docker/Dockerfile.websocket" "anthias-websocket" "$TAG"
+# build_and_push "docker/Dockerfile.celery" "anthias-celery" "$TAG"
+# build_and_push "docker/Dockerfile.redis" "anthias-redis" "$TAG"
+# build_and_push "docker/Dockerfile.wifi-connect" "anthias-wifi-connect" "$TAG"
+
+echo "All images have been built and pushed successfully!"
